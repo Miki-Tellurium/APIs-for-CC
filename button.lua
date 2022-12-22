db48769d8d316a24dd687db104b53ec78bf2248f
Miki-Tellurium <michelegamer14@gmail.com>
2022-12-22 19:20:08 +0100

    Added documentation and minor fixes

--- button.lua
+++ button.lua
@@ -1,16 +1,23 @@
 --Button making API by Miki_Tellurium
 --Version 1.0
---To do: documentation
+local expect = require("cc.expect")
+local expect = expect.expect
 
 local dTextColor = colors.black         --default button text color
 local dActBackColor = colors.green      --default background color when active
-local dInactBAckColor = colors.red      --default background color when inactive
-local dDisBackColor = colors.lightGrey  --default background color when disabled
+local dInactBackColor = colors.red      --default background color when inactive
+local dDisBackColor = colors.lightGray  --default background color when disabled
 local dBlinkColor = colors.yellow       --default blink color
 active = "active"
 inactive = "inactive"
 disabled = "disabled"
 
+--- This API provide tools to draw and add functions to buttons.
+--- Buttons can have different state and change look based on the
+--- current state. The background and text color of the button is
+--- customizable using the ComputerCraft [colors] API. You can also
+--- use [button:waitForClick] to check when a button is clicked and
+--- add functions to it.
 ---@class button
 button = {
     label = "button",               --text displayed on the button
@@ -21,21 +28,31 @@ button = {
     inactTColor = dTextColor,       --text color when inactive
     disTColor = dTextColor,         --text color when disabled
     actBColor = dActBackColor,      --background color when active
-    inactBColor = dInactBAckColor,  --background color when inactive
+    inactBColor = dInactBackColor,  --background color when inactive
     disBColor = dDisBackColor,      --background color when disabled
     blinkColor = dBlinkColor,       --background color when button blink
     state = active,                 --possible states: active, inactive, disabled
     isBlink = false                 --if the button blink when clicked
 }
 
---Class setup, allow to create new button objects
-function button:new (obj)
+--- Create a new button with default values and return it.
+--- You can use it like this:
+--- * buttonName = [button:new()]
+---
+--- buttonName will be the name of the new button object, you
+--- can call it's functions like this:
+--- * buttonName:function()
+function button:new(obj)
     obj = obj or {}
     setmetatable(obj, self)
     self.__index = self
     return obj
 end
 
+--- Deletes the button from the terminal and erases its data.
+--- Once this function is called the button no longer exist.
+--- If you want to delete the button from the terminal without
+--- erasing its data use [button:clear].
 function button:delete()
     local background = term.getBackgroundColor()
     self:setBackgroundColor(background)
@@ -44,105 +61,72 @@ function button:delete()
     self = {}
 end
 
+--- Set the text that will be displayed on the button.
+---@param text string The text to be displayed on the button
 function button:setLabel(text)
+    expect(1, text, "string")
     self.label = tostring(text)
 end
 
+--- Returns the label of the button.
+---@return string The current label of the button
 function button:getLabel()
     return self.label
 end
 
+--- Set the width of the button. If the width is greater than
+--- the number of characters on the label than the label on the
+--- button will be centered. Throws if the width is lower than
+--- the number of characters on the label.
+---@param int number The width of the button
 function button:setWidth(int)
+    expect(1, int, "number")
     if int < #self.label then
-        error("Width must be equal or superior to number of characters in label")
+        error("bad argument to setWidth (can't be lower than label size)", 2)
     end
     self.width = int
 end
 
+--- Returns the width of the button
+---@return number The width of the button
 function button:getWidth()
     return self.width
 end
 
+--- Set where the button will be displayed on the terminal.
+---@param x number The x coordinate of the button
+---@param y number The y coordinate of the button
 function button:setLocation(x, y)
+    expect(1, x, "number")
+    expect(2, y, "number")
     self.x = x
     self.y = y
 end
 
+--- Returns the x and y coordinates of the button
+---@return number The x and y coordinates of the button
 function button:getLocation()
     return self.x, self.y
 end
 
---Set same text color for all states
-function button:setTextColor(color)
-    self.actTColor = color
-    self.inactTColor = color
-    self.disTColor = color
-end
-
-function button:setActiveTextColor(color)
-    self.actTColor = color
-end
-
-function button:getActiveTextColor()
-    return self.actTColor
-end
-
-function button:setInactiveTextColor(color)
-    self.inactTColor = color
-end
-
-function button:getInactiveTextColor(color)
-    return self.inactTColor
-end
-
-function button:setDisabledTextColor(color)
-    self.disTColor = color
-end
-
-function button:getDisabledTextColor()
-    return self.disTColor
-end
-
---Set same background color for all states
-function button:setBackgroundColor(color)
-    self.actBColor = color
-    self.inactBColor = color
-    self.disBColor = color
-end
-
-function button:setActiveBackgroundColor(color)
-    self.actBColor = color
-end
-
-function button:getActiveBackgroundColor()
-    return self.actBColor
-end
-
-function button:setInactiveBackgroundColor(color)
-    self.inactBColor = color
-end
-
-function button:getInactiveBackgroundColor()
-    return self.inactBColor
-end
-
-function button:setDisabledBackgroundColor(color)
-    self.disBColor = color
-end
-
-function button:getDisabledBackgroundColor()
-    return self.disBColor
-end
-
-function button:setState(string, repaint)
-    if string == active then
+--- Set the current state of the button. Different states dictate
+--- the label and background color of the button. If repaint is true
+--- the button will be repainted on the terminal.
+--- Possible states are: active, inactive and disabled.
+--- Throws if the state is invalid.
+---@param state string The state of the button
+---@param repaint boolean If the button should be repainted on the terminal
+function button:setState(state, repaint)
+    expect(1, state, "string")
+    expect(2, repaint, "boolean", "nil")
+    if state == active then
         self.state = active
-    elseif string == inactive then
+    elseif state == inactive then
         self.state = inactive
-    elseif string == disabled then
+    elseif state == disabled then
         self.state = disabled
     else
-        error("Incorrect state value. Possible states are active, inactive, disabled")
+        error("bad argument to setState (possible states: active, inactive, disabled)", 2)
     end
 
     if repaint then
@@ -150,26 +134,137 @@ function button:setState(string, repaint)
     end
 end
 
+--- Returns the current state of the button.
+---@return string The state of the button
 function button:getState()
     return self.state
 end
 
+--- Set the same text color for all states.
+---@param color number The color to set the text to
+function button:setTextColor(color)
+    expect(1, color, "number")
+    self.actTColor = color
+    self.inactTColor = color
+    self.disTColor = color
+end
+
+--- Set the text color for the active state.
+---@param color number The color to set the text to
+function button:setActiveTextColor(color)
+    expect(1, color, "number")
+    self.actTColor = color
+end
+
+--- Returns the current text color for the active state of the button.
+---@return number The current text color for the active state
+function button:getActiveTextColor()
+    return self.actTColor
+end
+
+--- Set the text color for the inactive state.
+---@param color number The color to set the text to
+function button:setInactiveTextColor(color)
+    expect(1, color, "number")
+    self.inactTColor = color
+end
+
+--- Returns the current text color for the inactive state of the button.
+---@return number The current text color for the inactive state
+function button:getInactiveTextColor()
+    return self.inactTColor
+end
+
+--- Set the text color for the disabled state.
+---@param color number The color to set the text to
+function button:setDisabledTextColor(color)
+    expect(1, color, "number")
+    self.disTColor = color
+end
+
+--- Returns the current text color for the disabled state of the button.
+---@return number The current text color for the disabled state
+function button:getDisabledTextColor()
+    return self.disTColor
+end
+
+--- Set the same background color for all states.
+---@param color number The color to set the background to
+function button:setBackgroundColor(color)
+    expect(1, color, "number")
+    self.actBColor = color
+    self.inactBColor = color
+    self.disBColor = color
+end
+
+--- Set the background color for the active state.
+---@param color number The color to set the background to
+function button:setActiveBackgroundColor(color)
+    expect(1, color, "number")
+    self.actBColor = color
+end
+
+--- Returns the current background color for the active state of the button.
+---@return number The current background color for the active state
+function button:getActiveBackgroundColor()
+    return self.actBColor
+end
+
+--- Set the background color for the inactive state.
+---@param color number The color to set the background to
+function button:setInactiveBackgroundColor(color)
+    expect(1, color, "number")
+    self.inactBColor = color
+end
+
+--- Returns the current background color for the inactive state of the button.
+---@return number The current background color for the inactive state
+function button:getInactiveBackgroundColor()
+    return self.inactBColor
+end
+
+--- Set the background color for the disabled state.
+---@param color number The color to set the background to
+function button:setDisabledBackgroundColor(color)
+    expect(1, color, "number")
+    self.disBColor = color
+end
+
+--- Returns the current background color for the disabled state of the button.
+---@return number The current background color for the disabled state
+function button:getDisabledBackgroundColor()
+    return self.disBColor
+end
+
+--- Set if the button should blink when clicked.
+---@param boolean boolean If the button should blink when clicked
 function button:setBlinking(boolean)
+    expect(1, boolean, "boolean")
     self.isBlink = boolean
 end
 
-function button:isBlinking()
+--- Returns true if the button is set to blink when clicked.
+---@return boolean If the button is set to blink when clikced
+function button:doesBlink()
     return self.isBlink
 end
 
+--- Set the color the button should turn to when clicked.
+---@param color number The color the button should turn to when blinking
 function button:setBlinkColor(color)
+    expect(1, color, "number")
     self.blinkColor = color
 end
 
+--- Returns the color the button is set to turn to when blinking.
+---@return number The color the button turn to when blinking
 function button:getBlinkColor()
     return self.blinkColor
 end
 
+--- Draw the button on the screen using the current button
+--- properties. Note that after the function is called the
+--- cursor return where it was before drawing the button.
 function button:paint()
     --Store current text and background color for later
     local cText = term.getTextColor()
@@ -211,10 +306,12 @@ function button:paint()
     term.setCursorPos(cX, cY)
 end
 
+--- Make the button blink. This function is called by [button:waitForClick]
+--- if [button:setBlinking] is set to true.
 function button:blink()
     local blinkColor = self.blinkColor
     local cBack = nil
-    if self:isBlinking() then
+    if self:doesBlink() then
         if self.state == active then
             cBack = self.actBColor
         elseif self.state == inactive then
@@ -246,6 +343,9 @@ function button:blink()
     end
 end
 
+--- Delete the button from the terminal. It doesn't erase its data.
+--- Note that this function use the current background color of the
+--- terminal to delete the button.
 function button:clear()
     local background = term.getBackgroundColor()
     local cText = nil
@@ -286,7 +386,13 @@ function button:clear()
     end
 end
 
+--- Return true if the button was clicked. This function checks for
+--- the [mouse_click] or [monitor_touch] events and returns true if
+--- the button was clicked. You can pass an event to this function
+--- by using [os.pullEvent] in a table:
+--- - local event = {[os.pullEvent()]}
 function button:waitForClick(event)
+    expect(1, event, "table")
     if event[1] == "mouse_click" and event[2] == 1 then
         px, py = event[3], event[4]
         if px >= self.x and px < self.x+self.width and py == self.y then
